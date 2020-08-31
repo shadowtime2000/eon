ec66401ffe2551daa686f7fceba44c065061346b
Jake <pygamer138@gmail.com>
2020-08-31 11:42:04 +0200

    create json demo

--- demo/index.js
+++ demo/index.js
@@ -2,4 +2,5 @@ const turbo = require('..')(8080);
 
 turbo.get('/').text((req, res) => 'Faster test');
 turbo.get('/hook').hook((req, res) => res.end(`You requested ${req.pathname}`));
+turbo.get('/json').json((req, res) => ({hello: 'world'}));
 turbo.listen(p => console.log(`Listening on http://localhost:${p}`));
