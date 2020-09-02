28d744c99ffe520a75efbfd01c2e6ce98691e80e
Jake <pygamer138@gmail.com>
2020-09-02 10:43:21 +0200

    fix critical bug with debug helper

--- libs/debug.js
+++ libs/debug.js
@@ -1,6 +1,6 @@
 module.exports =  function (name) {
     return function(...msg) {
-        if (process.env.DEBUG.split(',').includes(name)) {
+        if ((process.env.DEBUG || '').split(',').includes(name)) {
             let color = `\x1b[38;5;${(Math.abs((name + " ").split('').reduce((p, idx) => ((typeof p === 'string') ? p.charCodeAt(0) : p) + name.charCodeAt(idx)) - 1) % 256)};1m`;
             return console.error(color + ' ', name + '\x1b[0m', ...msg);
         }
