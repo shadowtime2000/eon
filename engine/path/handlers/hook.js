c118fb201519464cb16254e286d4ee8816f10422
Jake <pygamer138@gmail.com>
2020-09-08 16:25:26 +0200

    #5 send content-type if not yet set

--- engine/path/handlers/hook.js
+++ engine/path/handlers/hook.js
@@ -2,8 +2,8 @@ const BaseHandler = require('./text');
 const { IncommingHTTPData, OutgoingHTTPData } = require('./streams');
 
 class HookCallbackHandler extends BaseHandler {
-    invoke(req, res, noParseBody) {
-        const Req = new IncommingHTTPData(req, noParseBody);
+    invoke(req, res, options) {
+        const Req = new IncommingHTTPData(req, options.noParseBody);
         if (Req.error) return res.writeHead(400).end('Invalid request: ' + Req.error.message);
         const Res = new OutgoingHTTPData(res);
         this.callback(Req, Res);
--- engine/path/handlers/json.js
+++ engine/path/handlers/json.js
@@ -2,11 +2,13 @@ const BaseHandler = require('./text');
 const { IncommingHTTPData, OutgoingHTTPData } = require('./streams');
 
 class JSONCallbackHandler extends BaseHandler {
-    invoke(req, res, noParseBody) {
-        const Req = new IncommingHTTPData(req, noParseBody);
+    invoke(req, res, options) {
+        const Req = new IncommingHTTPData(req, options.noParseBody);
         if (Req.error) return res.writeHead(400).end('Invalid request: ' + Req.error.message);
         const Res = new OutgoingHTTPData(res);
-        if(!Res.ended) Res.end(JSON.stringify(this.callback(Req, Res)));
+        const data = this.callback(Req, Res);
+        if (!Res.getHeader('content-type')) Res.header('content-type', `application/json${options.noUTF8Header ? '': '; charset=utf-8'}`);
+        if(!Res.ended) Res.end(JSON.stringify(data));
     }
 }
 
